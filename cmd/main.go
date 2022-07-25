a673bfb061d86e02a18018d60d67b6fb480b1a3d
fancy-w-w-w <1422916061@qq.com>
2022-07-25 16:26:38 +0800

    shanghai/7.25

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"math/rand"
+	"time"
+
+	"github.com/umpc/go-sortedmap"
+	"github.com/umpc/go-sortedmap/asc"
 )
 
 // func main() {
@@ -85,7 +87,30 @@ import (
 // }
 
 func main() {
-	for i := 0; i < 10; i++ {
-		fmt.Println(rand.Int63n(int64(math.Pow10(10))*9) + int64(math.Pow10(10)))
+	// Create an empty SortedMap with a size suggestion and a less than function:
+	sm := sortedmap.New(4, asc.Time)
+
+	// Insert example records:
+	sm.Insert("OpenBSD", time.Date(1995, 10, 18, 8, 37, 1, 0, time.UTC))
+	sm.Insert("UnixTime", time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC))
+	sm.Insert("Linux", time.Date(1991, 8, 25, 20, 57, 8, 0, time.UTC))
+	sm.Insert("GitHub", time.Date(2008, 4, 10, 0, 0, 0, 0, time.UTC))
+
+	// Set iteration options:
+	reversed := false
+	lowerBound := time.Date(1994, 1, 1, 0, 0, 0, 0, time.UTC)
+	upperBound := time.Now()
+
+	// Select values > lowerBound and values <= upperBound.
+	// Loop through the values, in reverse order:
+	iterCh, err := sm.BoundedIterCh(reversed, lowerBound, upperBound)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer iterCh.Close()
+
+	for rec := range iterCh.Records() {
+		fmt.Printf("%+v\n", rec)
 	}
 }
--- go.mod
+++ go.mod
@@ -10,6 +10,8 @@ require (
 require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/iancoleman/orderedmap v0.2.0 // indirect
+	github.com/umpc/go-sortedmap v0.0.0-20180422175548-64ab94c482f4 // indirect
 	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
 	golang.org/x/sys v0.0.0-20220406163625-3f8b81556e12 // indirect
 	golang.org/x/text v0.3.7 // indirect
